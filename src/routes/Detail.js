6f637c7a70a9e2490dc066fd9c34d9223c9f1489
sam <3161728@gmail.com>
2020-12-11 01:43:41 +0900

    update detail component

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -8,9 +8,27 @@ class Detail extends React.Component {
     }
   }
   render() {
-    const { location } = this.props;
+    const location = this.props.location;
+    const { year, title, summary, poster, genres } = location.state;
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      // return <span>{location.state.title}</span>;
+      return (
+        <span>
+          <img src={poster} alt={title} title={title} />
+          <div className="movie__data">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className="movie__genres">
+              {genres.map((genre, index) => (
+                <li key={index} className="genres__genre">
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p className="movie__summary">{summary}</p>
+          </div>
+        </span>
+      );
     } else {
       return null;
     }
