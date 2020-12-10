cae3fc6f56bea9161de8b6ab4187b9a85aad2dee
sam <3161728@gmail.com>
2020-12-10 19:37:22 +0900

    Test jsx props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food({ fav }) {
+  return <h1>I like {fav}!</h1>;
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
-      <Potato />
+      <h1>Hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="apple" />
+      <Food fav="orange" />
     </div> 
   );
 }
