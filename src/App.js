e99961deb24ccdf77f02bd1d36ebcb2d321dd18b
sam <3161728@gmail.com>
2020-12-10 22:17:33 +0900

    Install axios package

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = { 
@@ -6,16 +7,19 @@ class App extends React.Component {
     movies: [] 
   };
 
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    console.log(movies);
+  }
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false});
-    }, 6000);
+    this.getMovies();
   }
 
   render() {
     const { isLoading } = this.state;
     return (
-    <div>{isLoading ? "Loading..." : "We are ready"}</div>
+      <div>{isLoading ? "Loading..." : "We are ready"}</div>
     );
   }
 }
