2bb17c13b18ee6c2e345fff4447a616ec90b48ff
sam <3161728@gmail.com>
2020-12-10 20:31:21 +0900

    add key and alt prop

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
 import React from 'react';
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} />
-    </div>
-  );
-}
-
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
+    id: 2,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
   },
   {
+    id: 3,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
   },
   {
+    id: 4,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
   }
 ];
 
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name}/>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      {foodILike.map(x => <Food name={x.name} picture={x.image} />)}
+      {foodILike.map(x => (<Food key={x.id} name={x.name} picture={x.image} />))}
     </div> 
   );
 }
