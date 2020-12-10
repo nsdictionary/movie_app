e45689406248e5bb9675106882650326d982068e
sam <3161728@gmail.com>
2020-12-11 01:35:23 +0900

    Define redirecting

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types"
 import "./Movie.css";
 
-function Movie({year, title, summary, poster, genres}) {
+function Movie({id, year, title, summary, poster, genres}) {
     return (
+        <div className="movie">
         <Link to={{
-            pathname: "/movie-detail",
+            pathname: `/movie/${id}`,
             state: { year, title, summary, poster, genres }
         }}>
-        <div className="movie">
             <img src={poster} alt={title} title={title} />
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
@@ -23,8 +23,8 @@ function Movie({year, title, summary, poster, genres}) {
                 </ul>
                 <p className="movie__summary">{summary.slice(0, 180)}...</p>
             </div>
-        </div>
         </Link>
+        </div>
     );
 }
 
